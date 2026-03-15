0146edb5ff8ee1adef0840015e4c9f1c96567328
Mustafa Efe Tamer <119308432+MustafaEfeTamer@users.noreply.github.com>
2026-03-15 15:11:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
@@ -12,6 +12,6 @@ def home():
 
 @app.route("/metrics")
 def metrics():
-    return generate_latest()
+    return Response(generate_latest(), mimetype="text/plain")
 
 app.run(host="0.0.0.0", port=5000)
